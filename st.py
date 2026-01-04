16438748dbf62db4fd25554c0f84bf6a3ccd5e0a
bloxxastro1 <126952221+bloxxastro1@users.noreply.github.com>
2026-01-04 13:45:44 +0200

    Update st.py

--- st.py
+++ st.py
@@ -4,7 +4,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import plotly as px 
+import plotly.express as px 
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split, cross_validate, StratifiedKFold
@@ -345,6 +345,7 @@ st.pyplot(fig)
 
 roc_auc = roc_auc_score(y_test, y_proba)
 st.write(f"ROC-AUC Score: {roc_auc:.4f}")
+
 
 
 
