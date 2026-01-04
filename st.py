d8b15eb8bbd9cb9458789d67a9939f959494c8ee
bloxxastro1 <126952221+bloxxastro1@users.noreply.github.com>
2026-01-04 13:16:22 +0200

    Update st.py

--- st.py
+++ st.py
@@ -87,7 +87,7 @@ st.subheader("Univariate Analysis")
 num_cols = df.select_dtypes(include=['int64', 'float64']).columns.tolist()
 
 for col in num_cols:
-    fig = plt.histogram(df, x=col, title=f"Distribution of {col}")
+    fig = plt.hist(df, x=col, title=f"Distribution of {col}")
     st.plotly_chart(fig, use_container_width=True)
 
 # ===============================
@@ -214,6 +214,7 @@ st.pyplot(fig)
 
 roc_auc = roc_auc_score(y_test, y_proba)
 st.write(f"ROC-AUC Score: {roc_auc:.4f}")
+
 
 
 
